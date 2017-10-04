0c55e5b9112b9a2e9118f50f2c04dd30e856fc59
Yang Liu <hi@zesik.com>
2017-10-05 08:04:47 +0900

    Update Pane tests

--- test/Pane.spec.jsx
+++ test/Pane.spec.jsx
@@ -1,19 +1,16 @@
-import expect from 'expect';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import Pane from '../src/components/Pane';
 
-function setupPane(content, props) {
-  props = props || {};
-  const renderer = TestUtils.createRenderer();
+function render(content, props = {}) {
+  const renderer = new ShallowRenderer();
   renderer.render(<Pane {...props}>{content}</Pane>);
-  const output = renderer.getRenderOutput();
-  return { props, output, renderer };
+  return renderer.getRenderOutput();
 }
 
 describe('Pane', () => {
   it('should render a Pane correctly', () => {
-    const { output } = setupPane('test pane');
+    const output = render('test pane');
     expect(output.type).toBe('div');
     expect(output.props.className).toBe('layout-pane');
     expect(output.props.style).toEqual({ width: '0px' });
@@ -21,7 +18,7 @@ describe('Pane', () => {
   });
 
   it('should render properties of a Pane correctly if requested', () => {
-    const { output } = setupPane('test pane', { vertical: true, size: 2, percentage: true });
+    const output = render('test pane', { vertical: true, size: 2, percentage: true });
     expect(output.type).toBe('div');
     expect(output.props.className).toBe('layout-pane');
     expect(output.props.style).toEqual({ height: '2%' });
@@ -29,7 +26,7 @@ describe('Pane', () => {
   });
 
   it('should render a primary Pane correctly if requested', () => {
-    const { output } = setupPane('test pane', { primary: true, vertical: true, size: 2, percentage: true });
+    const output = render('test pane', { primary: true, vertical: true, size: 2, percentage: true });
     expect(output.type).toBe('div');
     expect(output.props.className).toBe('layout-pane layout-pane-primary');
     expect(output.props.style).toEqual({});
