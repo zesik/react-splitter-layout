cb8d2df6fd106287727e9fa4e9e842f196e47cb3
Yang Liu <hi@zesik.com>
2017-10-05 08:00:23 +0900

    Update webpack config for version 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 const webpack = require('webpack');
 
 module.exports = {
@@ -5,28 +6,34 @@ module.exports = {
     './index.js'
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css']
+    extensions: ['.js', '.jsx', '.css']
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') })
   ],
   output: {
-    path: 'lib',
+    path: resolve(__dirname, 'lib'),
     filename: 'index.js',
     library: 'react-splitter-layout',
     libraryTarget: 'umd'
   },
   externals: {
-    react: 'react'
+    react: 'react',
+    'prop-types': 'prop-types'
   }
 };
