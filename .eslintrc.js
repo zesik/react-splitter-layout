80c96d46d6ce57319efcfd019973ca7b9a721fdc
Yang Liu <hi@zesik.com>
2018-04-28 13:08:49 +0900

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,13 @@ module.exports = {
   },
   'rules': {
     'comma-dangle': [2, 'never'],
-    'func-names': [0],
+    "function-paren-newline": [2, 'consistent'],
     'max-len': [2, 120, 2, {
       'ignoreUrls': true
     }],
     'no-multiple-empty-lines': [1, { 'max': 1 }],
     'no-plusplus': [0],
+    'object-curly-newline': [0],
     'space-before-function-paren': [2, 'never'],
     'react/no-did-mount-set-state': [0],
     'jsx-a11y/no-noninteractive-element-interactions': [0]
