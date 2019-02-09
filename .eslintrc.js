9effded44a0fdd90d8a0e71fc7644046a1c74a88
Yang Liu <hi@zesik.com>
2019-02-09 15:07:20 +0900

    Upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,11 @@ module.exports = {
     'no-multiple-empty-lines': [1, { 'max': 1 }],
     'no-plusplus': [0],
     'object-curly-newline': [0],
+    'operator-linebreak': [2, 'after'],
     'space-before-function-paren': [2, 'never'],
+    'react/destructuring-assignment': [0],
     'react/no-did-mount-set-state': [0],
+    'react/jsx-one-expression-per-line': [0],
     'jsx-a11y/no-noninteractive-element-interactions': [0]
   }
 };
