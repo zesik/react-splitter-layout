0d2580e76ef34b9d862bc2e67e5db8888ee30dcd
Sebastian Frey <sebastian.frey@outlook.com>
2019-12-02 15:58:04 +0100

    Added secondaryHidden prop

--- example/javascripts/components/TogglableSidebarLayout.jsx
+++ example/javascripts/components/TogglableSidebarLayout.jsx
@@ -6,8 +6,10 @@ export default class TogglableSidebarLayout extends React.Component {
   constructor(props) {
     super(props);
     this.toggleSidebar = this.toggleSidebar.bind(this);
+    this.toggleUseSecondaryHidden = this.toggleUseSecondaryHidden.bind(this);
     this.state = {
-      sidebarVisible: true
+      sidebarVisible: true,
+      useSecondaryHidden: false
     };
   }
 
@@ -15,9 +17,17 @@ export default class TogglableSidebarLayout extends React.Component {
     this.setState(state => ({ sidebarVisible: !state.sidebarVisible }));
   }
 
+  toggleUseSecondaryHidden() {
+    this.setState(state => ({ useSecondaryHidden: !state.useSecondaryHidden }));
+  }
+
   render() {
     return (
-      <SplitterLayout percentage secondaryInitialSize={25}>
+      <SplitterLayout
+        percentage
+        secondaryInitialSize={25}
+        secondaryHidden={this.state.useSecondaryHidden ? !this.state.sidebarVisible : undefined}
+      >
         <div className="my-pane">
           <h2>1st Pane</h2>
           <p>This is the 1st pane, and this is the primary pane by default.</p>
@@ -25,29 +35,50 @@ export default class TogglableSidebarLayout extends React.Component {
             {this.state.sidebarVisible && 'Hide Sidebar'}
             {!this.state.sidebarVisible && 'Show Sidebar'}
           </button>
-          <pre>
-            &lt;SplitterLayout primaryIndex={'{0}'}&gt;{'\n'}
-            &nbsp;&nbsp;<strong>&lt;div&gt;1st&lt;/div&gt;</strong>{'\n'}
-            {this.state.sidebarVisible && '  <div>2nd</div>\n'}
-            &lt;/SplitterLayout&gt;
-          </pre>
+          <button type="button" style={{ marginLeft: 16 }} onClick={this.toggleUseSecondaryHidden}>
+            {this.state.useSecondaryHidden && 'Use secondaryHidden prop to hide secondary pane'}
+            {!this.state.useSecondaryHidden && 'Use conditional rendering to hide secondary pane'}
+          </button>
+          {!this.state.useSecondaryHidden && (
+            <pre>
+              &lt;SplitterLayout primaryIndex={'{0}'}&gt;{'\n'}
+              &nbsp;&nbsp;<strong>&lt;div&gt;1st&lt;/div&gt;</strong>{'\n'}
+              {this.state.sidebarVisible && '  <div>2nd</div>\n'}
+              &lt;/SplitterLayout&gt;
+            </pre>
+          )}
+          {this.state.useSecondaryHidden && (
+            <pre>
+              &lt;SplitterLayout primaryIndex={'{0}'} secondaryHidden={`{${!this.state.sidebarVisible}}`}&gt;{'\n'}
+              &nbsp;&nbsp;<strong>&lt;div&gt;1st&lt;/div&gt;</strong>{'\n'}
+              {'  <div>2nd</div>\n'}
+              &lt;/SplitterLayout&gt;
+            </pre>
+          )}
+
           <Lorem title="1st Pane" />
         </div>
-        {this.state.sidebarVisible &&
-          (
-            <div className="my-pane">
-              <h2>2nd Pane</h2>
-              <p>This is the 2nd pane, considered as a sidebar.</p>
-              <pre>
-                &lt;SplitterLayout primaryIndex={'{0}'}&gt;{'\n'}
-                &nbsp;&nbsp;&lt;div&gt;1st&lt;/div&gt;{'\n'}
-                &nbsp;&nbsp;<strong>&lt;div&gt;2nd&lt;/div&gt;</strong>{'\n'}
-                &lt;/SplitterLayout&gt;
-              </pre>
-              <Lorem title="2nd Pane" />
-            </div>
-          )
-        }
+        <div className="my-pane">
+          <h2>2nd Pane</h2>
+          <p>This is the 2nd pane, considered as a sidebar.</p>
+          {!this.state.useSecondaryHidden && (
+            <pre>
+              &lt;SplitterLayout primaryIndex={'{0}'}&gt;{'\n'}
+              &nbsp;&nbsp;&lt;div&gt;1st&lt;/div&gt;{'\n'}
+              &nbsp;&nbsp;<strong>&lt;div&gt;2nd&lt;/div&gt;</strong>{'\n'}
+              &lt;/SplitterLayout&gt;
+            </pre>
+          )}
+          {this.state.useSecondaryHidden && (
+            <pre>
+              &lt;SplitterLayout primaryIndex={'{0}'} secondaryHidden={`{${!this.state.sidebarVisible}}`}&gt;{'\n'}
+              &nbsp;&nbsp;&lt;div&gt;1st&lt;/div&gt;{'\n'}
+              &nbsp;&nbsp;<strong>&lt;div&gt;2nd&lt;/div&gt;</strong>{'\n'}
+              &lt;/SplitterLayout&gt;
+            </pre>
+          )}
+          <Lorem title="2nd Pane" />
+        </div>
       </SplitterLayout>
     );
   }
--- src/components/Pane.jsx
+++ src/components/Pane.jsx
@@ -4,10 +4,13 @@ import PropTypes from 'prop-types';
 function Pane(props) {
   const size = props.size || 0;
   const unit = props.percentage ? '%' : 'px';
+  const hidden = props.hidden || false;
   let classes = 'layout-pane';
   const style = {};
   if (!props.primary) {
-    if (props.vertical) {
+    if (hidden) {
+      style.display = 'none';
+    } else if (props.vertical) {
       style.height = `${size}${unit}`;
     } else {
       style.width = `${size}${unit}`;
@@ -24,6 +27,7 @@ Pane.propTypes = {
   vertical: PropTypes.bool,
   primary: PropTypes.bool,
   size: PropTypes.number,
+  hidden: PropTypes.bool,
   percentage: PropTypes.bool,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
@@ -35,6 +39,7 @@ Pane.defaultProps = {
   vertical: false,
   primary: false,
   size: 0,
+  hidden: false,
   percentage: false,
   children: []
 };
