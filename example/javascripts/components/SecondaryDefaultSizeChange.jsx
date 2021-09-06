d4eb877ff81efdec098ddc9538cc5227b00ea20b
Chase Gibbons <chase3111@gmail.com>
2021-09-05 23:16:51 -0600

    clean code

--- example/javascripts/components/SecondaryDefaultSizeChange.jsx
+++ example/javascripts/components/SecondaryDefaultSizeChange.jsx
@@ -22,62 +22,22 @@ export default class TogglableSidebarLayout extends React.Component {
 
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          // gridTemplateAreas: "s b b b b b",
-          // gridTemplateColumns: "1fr",
-          // gridTemplateRows: "auto",
-          height: "100%",
-          width: "100%",
-          justifyContent: "space-between"
-        }}
-      >
+      <div style={{display: "flex",height: "100%",width: "100%",justifyContent: "space-between"}}>
         <div className="my-sidebar" style={{ flex: 1, height: "100%" }}>
           <ul>
-            <li>
-              <a onClick={this.handleSidebarItemClick.bind(this, 0)}>
-                First View
-              </a>
-            </li>
-            <li>
-              <a onClick={this.handleSidebarItemClick.bind(this, 1)}>
-                Second View
-              </a>
-            </li>
+            {['First View', 'Second View'].map(((li, idx) => (
+              <li><a onClick={this.handleSidebarItemClick.bind(this, idx)}>{li}</a></li>
+            )))}
           </ul>
         </div>
         <div style={{ flex: 4, height: "100%" }}>
-          <SplitterLayout
-            percentage
-            secondaryInitialSize={this.state.activeIndex === 0 ? 25 : 75}
-            vertical={true}
-          >
+          <SplitterLayout percentage secondaryInitialSize={this.state.activeIndex === 0 ? 25 : 75} vertical={true}>
             <div>
               <h2>1st Pane</h2>
-              <p>
-                This is the 1st pane, and this is the primary pane by default.
-              </p>
-
-              <pre>
-                &lt;SplitterLayout primaryIndex={"{0}"}&gt;{"\n"}
-                &nbsp;&nbsp;<strong>&lt;div&gt;1st&lt;/div&gt;</strong>
-                {"\n"}
-                {this.state.sidebarVisible && "  <div>2nd</div>\n"}
-                &lt;/SplitterLayout&gt;
-              </pre>
               <Lorem title="1st Pane" />
             </div>
             <div>
               <h2>2nd Pane</h2>
-              <p>This is the 2nd pane, considered as a sidebar.</p>
-              <pre>
-                &lt;SplitterLayout primaryIndex={"{0}"}&gt;{"\n"}
-                &nbsp;&nbsp;&lt;div&gt;1st&lt;/div&gt;{"\n"}
-                &nbsp;&nbsp;<strong>&lt;div&gt;2nd&lt;/div&gt;</strong>
-                {"\n"}
-                &lt;/SplitterLayout&gt;
-              </pre>
               <Lorem title="2nd Pane" />
             </div>
           </SplitterLayout>
